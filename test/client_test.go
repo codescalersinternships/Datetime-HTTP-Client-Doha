8a9172749bb2f0fadce59d4d6e9cc75e9ab09310
dohaelsawy <dohaelsawy18@gmail.com>
2024-09-17 17:48:14 +0300

    feat: add unittest for client functions

---
+++ test/client_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+
+	pkg "github.com/dohaelsawy/codescalers/datetime-client/pkg"
+	"github.com/go-playground/assert/v2"
+)
+
+
+type MockClient struct {
+	url string
+}
+
+
+
+func TestClient(t *testing.T){
+
+	t.Run("default client configs", func(t *testing.T) {
+		mconfig := pkg.SetConfigDefualt()
+
+		client := pkg.NewClient(mconfig)
+
+		expect :=  MockClient{
+			url: "http://localhost:8080/datetime",
+		}
+
+		assert.Equal(t, expect.url, client.Url)
+	})
+
+	t.Run("client with .env configs", func(t *testing.T) {
+		mconfig := pkg.LoadConfigFromENV()
+
+		client := pkg.NewClient(mconfig)
+
+		expect:= &MockClient{
+			url: "http://localhost:8090/datetime",
+		}
+
+		assert.Equal(t, expect.url, client.Url)
+	})
+}
